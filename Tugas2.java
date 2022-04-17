430cd7de78114d22b7793acf099ded6c0f04925b
Raihan Abigail <101303595+20104205@users.noreply.github.com>
2022-04-18 00:17:39 +0800

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,14 @@
+
+package T2;
+
+/**
+ *
+ * @sultanaderamadhani 20104221
+ */
+public class Tugas2 {
+    public static void main(String[] args) {
+        volumebalok balok1 = new volumebalok(9,3,8);
+        balok1.cetakvolume ();
+    }
+    
+}
